dc824ad86ce70438bfa1ce3b49cdea8f0ec2cd8d
Kanwar27 <kanwarpreet@live.in>
2016-08-19 01:11:02 -0400

    Finally separated in label data format.

--- FinalProj2.7/downloadData.py
+++ FinalProj2.7/downloadData.py
@@ -36,7 +36,7 @@ def getTextFromCorpus(corpus):
         try:
             status = api.GetStatus(tweet["tweet_id"])
             tweet["text"] = status.text
-            print(tweet["text"])
+            print(tweet["tweet_id"] + " : " + tweet["text"])
             trainingData.append(tweet)
             time.sleep(sleep_time) # to avoid being rate limited
         except:
@@ -62,67 +62,67 @@ def saveAfterDownload(trainingData, tweetDataFile):
                     print e
         # return trainingData
 
-corpusFile1 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.NON_SARCASM.all.id.DEV'
-corpusFile2 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.NON_SARCASM.all.id.TEST'
-corpusFile3 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.NON_SARCASM.all.id.TRAIN'
+#corpusFile1 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.NON_SARCASM.all.id.DEV'
+#corpusFile2 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.NON_SARCASM.all.id.TEST'
+#corpusFile3 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.NON_SARCASM.all.id.TRAIN'
 corpusFile4 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SARCASM.all.id.DEV'
 corpusFile5 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SARCASM.all.id.TEST'
-corpusFile6 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SARCASM.all.id.TRAIN'
+#corpusFile6 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SARCASM.all.id.TRAIN'
 corpusFile7 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SENTIMENT.all.id.DEV'
 corpusFile8 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SENTIMENT.all.id.TEST'
-corpusFile9 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SENTIMENT.all.id.TRAIN'
+#corpusFile9 = r'E:\NLP_SarcasmDetector\FinalProj2.7\Data\tweet.SENTIMENT.all.id.TRAIN'
 
-readCorpus(corpusFile1, corpus1)
-readCorpus(corpusFile2, corpus2)
-readCorpus(corpusFile3, corpus3)
+#readCorpus(corpusFile1, corpus1)
+#readCorpus(corpusFile2, corpus2)
+#readCorpus(corpusFile3, corpus3)
 readCorpus(corpusFile4, corpus4)
 readCorpus(corpusFile5, corpus5)
-readCorpus(corpusFile6, corpus6)
+#readCorpus(corpusFile6, corpus6)
 readCorpus(corpusFile7, corpus7)
 readCorpus(corpusFile8, corpus8)
-readCorpus(corpusFile9, corpus9)
+#readCorpus(corpusFile9, corpus9)
 
-print("Reading first corpus !!")
-tweetDataFile1=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile1.csv'
-getTextFromCorpus(corpus1)
-saveAfterDownload(corpus1 , tweetDataFile1)
+# print("Reading first corpus !!")
+# tweetDataFile1=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile1.csv'
+# getTextFromCorpus(corpus1)
+# saveAfterDownload(corpus1 , tweetDataFile1)
+#
+# print("Reading second corpus !!")
+# tweetDataFile2=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile2.csv'
+# getTextFromCorpus(corpus2)
+# saveAfterDownload(corpus2 , tweetDataFile2)
 
-print("Reading second corpus !!")
-tweetDataFile2=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile2.csv'
-getTextFromCorpus(corpus2)
-saveAfterDownload(corpus2 , tweetDataFile2)
-
-print("Reading third corpus !!")
-tweetDataFile3=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile3.csv'
-getTextFromCorpus(corpus3)
-saveAfterDownload(corpus3 , tweetDataFile3)
+# print("Reading third corpus !!")
+# tweetDataFile3=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile3.csv'
+# getTextFromCorpus(corpus3)
+# saveAfterDownload(corpus3 , tweetDataFile3)
 
 print("Reading fourth corpus !!")
-tweetDataFile4=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile4.csv'
+tweetDataFile4=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile4_again.csv'
 getTextFromCorpus(corpus4)
 saveAfterDownload(corpus4 , tweetDataFile4)
 
 print("Reading fifth corpus !!")
-tweetDataFile5=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile5.csv'
+tweetDataFile5=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile5_again.csv'
 getTextFromCorpus(corpus5)
 saveAfterDownload(corpus5 , tweetDataFile5)
 
-print("Reading sixth corpus !!")
-tweetDataFile6=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile6.csv'
-getTextFromCorpus(corpus6)
-saveAfterDownload(corpus6 , tweetDataFile6)
+# print("Reading sixth corpus !!")
+# tweetDataFile6=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile6.csv'
+# getTextFromCorpus(corpus6)
+# saveAfterDownload(corpus6 , tweetDataFile6)
 
 print("Reading seventh corpus !!")
-tweetDataFile7=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile7.csv'
+tweetDataFile7=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile7_again.csv'
 getTextFromCorpus(corpus7)
 saveAfterDownload(corpus7 , tweetDataFile7)
 
 print("Reading eigth corpus !!")
-tweetDataFile8=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile8.csv'
+tweetDataFile8=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile8_again.csv'
 getTextFromCorpus(corpus8)
-saveAfterDownload(corpus8 , tweetDataFile8)
+saveAfterDownload(corpus8,tweetDataFile8)
 
-print("Reading ninth corpus !!")
-tweetDataFile9=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile9.csv'
-getTextFromCorpus(corpus9)
-saveAfterDownload(corpus9 , tweetDataFile9)
+# print("Reading ninth corpus !!")
+# tweetDataFile9=r'E:\\NLP_SarcasmDetector\\FinalProj2.7\\Data\\FetchedData\\tweetDataFile9.csv'
+# getTextFromCorpus(corpus9)
+# saveAfterDownload(corpus9 , tweetDataFile9)
