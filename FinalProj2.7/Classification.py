1b58755b43f88aeaeb902104c4a4654266430ea4
Kanwar27 <kanwarpreet@live.in>
2016-08-22 00:09:46 -0400

    data in chunks

--- FinalProj2.7/Classification.py
+++ FinalProj2.7/Classification.py
@@ -1,9 +1,12 @@
 import csv
 import twitter
-import re
-import os
+import os, time
 import sys, traceback
+import re
+import nltk
+from string import punctuation
 
+#credentials for connecting to twitter API.
 api = twitter.Api(
                     consumer_key = 'YnH8HtrVYhxmNSr4lLLpuI5MG',
                     consumer_secret = 'gk5Je6TJT9Bt2M7lrxask8LYFv0s4l7U2mKEeVUg6I7tvgAFW5',
@@ -11,6 +14,8 @@ api = twitter.Api(
                     access_token_secret = 'tE5wzDkRAijVNL5O6vfDQzHzyBWTGlEpvVN9Y6nhGACBz'
                 )
 
+_stopwords=[]
+#Append the current directory path to the file required.
 def getFullPath(fileName, isOutput=False):
     if(isOutput):
         dir = os.path.join(os.getcwd() ,'Data','FetchedData', 'FinalData', fileName)
@@ -18,7 +23,7 @@ def getFullPath(fileName, isOutput=False):
         dir = os.path.join(os.getcwd() ,'Data','FetchedData', fileName)
     return dir
 
-
+#Used for saving the corpus as file as soon as it is downloaded
 def saveAfterDownload(trainingData, tweetDataFile):
         with open(tweetDataFile, 'w') as csvfile:
             fieldnames = ['Topic', 'Text']
@@ -30,7 +35,7 @@ def saveAfterDownload(trainingData, tweetDataFile):
                 except:
                     continue
 
-
+#Read and Write used to fetch the corpus file and extract only emotion and text in new file.
 def readAndWriteCorpus(corpusFile, destFile):
     print(corpusFile)
     print(destFile)
@@ -50,6 +55,75 @@ def readAndWriteCorpus(corpusFile, destFile):
         traceback.print_exc(file=sys.stdout)
         print '-'*60
 
-#readAndWriteCorpus(getFullPath('tweetDataFile1.csv'), getFullPath('NON_SARCASM_DEV.txt',True))
-for _fileName in os.listdir(os.path.join(os.getcwd() ,'Data','FetchedData')):
-    print(readAndWriteCorpus(getFullPath(_fileName), getFullPath(_fileName,True)))
+
+#First processing of Tweets but the data needs to be sent as list parameter.
+def processTweets(list_of_tweets):
+    # The list of tweets is a list of dictionaries which should have the keys, "text" and "label"
+    processedTweets=[]
+    # This list will be a list of tuples. Each tuple is a tweet which is a list of words and its label
+    for tweet in list_of_tweets:
+        processedTweets.append((_processTweet(tweet.split(',')[1]),tweet.split(',')[0]))
+    return processedTweets
+
+#Second processing of Tweets
+def _processTweet(tweet):
+    # 1. Convert to lower case
+    tweet=tweet.lower()
+    # 2. Replace links with the word URL
+    tweet=re.sub('((www\.[^\s]+)|(https?://[^\s]+))','URL',tweet)
+    # 3. Replace @username with "AT_USER"
+    tweet=re.sub('@[^\s]+','AT_USER',tweet)
+    # 4. Replace #word with word
+    tweet=re.sub(r'#([^\s]+)',r'\1',tweet)
+
+    tweet=nltk.word_tokenize(tweet)
+    # This tokenizes the tweet into a list of words # Let's now return this list minus any stopwords
+    #return [word.replace("'","") for word in tweet if (word not in _stopwords and len(word)>= 3)]
+
+    tweet = [word.replace("'","") for word in tweet if (word not in _stopwords and len(word)>= 3)]
+
+    tweet=tagger.tag(tweet)
+
+    return tweet
+
+# Start Program Section :
+#
+# readAndWriteCorpus(getFullPath('tweetDataFile1.csv'), getFullPath('NON_SARCASM_DEV.txt',True))
+# for _fileName in os.listdir(os.path.join(os.getcwd() ,'Data','FetchedData')):
+#     print(readAndWriteCorpus(getFullPath(_fileName), getFullPath(_fileName,True)))
+
+from nltk.tag.perceptron import PerceptronTagger
+tagger = PerceptronTagger()
+
+#Getting a list for making stop words list.
+_stopwords=set(nltk.corpus.stopwords.words('english')+list(punctuation)+['AT_USER','URL','sarcasm'])
+allFileAsList=[]
+folderName = os.path.join(os.getcwd() ,'Data','FetchedData','FinalData')
+for _file in os.listdir(folderName):
+    #Printing fileName being scanned
+    # print '-'*100
+    # print(_file)
+    # print '-'*100
+
+    allFileAsList.append([])
+
+    #Opening file line by line and appending to list.
+    with open(getFullPath(_file,True),'rb') as csvfile:
+        lineReader = csv.reader(csvfile,delimiter=',')
+        for row in lineReader:
+            #allFileAsList[len(allFileAsList)-1].append('fileName : ' + _file)
+            allFileAsList[len(allFileAsList)-1].append(row[0] + ',' + row[1])
+
+
+# pro=[]
+# for eachList in allFileAsList:
+#     #print(eachList[0])   #format:always ,  Reading #zizek before bed is always a recipe for a nice
+#     pro=processTweets(_stopwords,eachList)
+
+devData=processTweets(allFileAsList[2])
+#testData=processTweets(allFileAsList[3])
+
+
+for data in devData:
+    print(data)
+
